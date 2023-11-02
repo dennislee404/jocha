260b95e330c06eb45c2e186f8e95f29e84e580d5
Dennis Lee <dennislee404@gmail.com>
2023-11-02 12:19:11 +0800

    cart item option

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -9,11 +9,11 @@ class CartItemsController < ApplicationController
 		end
 
 		if @cart.save
-			@cart_item = @cart.cart_items.where(item_id: @item.id)
+			@cart_item = @cart.cart_items.where(item_id: @item.id).last
 			@ice_level = OptionItem.find(params[:option_1])
 			@sugar_level = OptionItem.find(params[:option_2])
-			@cart_item_option = CartItemOption.create(cart_item: @cart_item, option: @ice_level)
-			@cart_item_option = CartItemOption.create(cart_item: @cart_item, option: @sugar_level)
+			@cart_item_option = CartItemOption.create(cart_item_id: @cart_item.id, option_item_id: @ice_level.id)
+			@cart_item_option = CartItemOption.create(cart_item: @cart_item, option_item: @sugar_level)
 			redirect_back fallback_location: root_path
 		else
 			redirect_to	product_path(params[:id]), notice: "fail to process"
@@ -32,3 +32,4 @@ class CartItemsController < ApplicationController
 		redirect_back fallback_location: root_path
 	end
 end
+

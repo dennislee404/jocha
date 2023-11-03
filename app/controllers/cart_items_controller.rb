9e462db56c38c5af3a974bc8d7898f252b233c63
Dennis Lee <dennislee404@gmail.com>
2023-11-03 14:20:05 +0800

    option price included

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -20,13 +20,13 @@ class CartItemsController < ApplicationController
 			end
 
 			#get cart_item_option prices
-			@cart_item_price = @cart_item.price
+			@cart_item_price = @cart_item.price_cents
 			@cart_item.cart_item_options.each do |opt|
-				@cart_item_price += opt.option_item.price 
+				@cart_item_price += opt.option_item.price*100
 			end
 
 			#update cart_item price
-			@cart_item.update(price: @cart_item_price)
+			@cart_item.update(price: @cart_item_price/100)
 			redirect_back fallback_location: root_path
 		else
 			redirect_to	product_path(params[:id]), notice: "fail to process"

1e6cfc1fbca380b6e827c08ebc3f95f7e89e6477
Dennis Lee <dennislee404@gmail.com>
2023-11-03 16:41:27 +0800

    cart item controller ok

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -9,16 +9,36 @@ class CartItemsController < ApplicationController
 		end
 
 		if @cart.save
-			# get cart_item	
+			# get cart_item
+			option_id = []	
 			@cart_item = @cart.cart_items.where(item_id: @item.id).last
-			if @cart_item.item_type
-			if params[:option_1].any?
-			#add cart_item_option
-			@cart_item_option = CartItemOption.create(cart_item_id: @cart_item.id, option_item_id: params[:option_1].to_i)
-			@cart_item_option = CartItemOption.create(cart_item_id: @cart_item.id, option_item_id: params[:option_2].to_i)
-			params[:option_3].each do |opt|
-				@cart_item_option = CartItemOption.create(cart_item_id: @cart_item.id, option_item_id: opt.to_i)
+			if @cart_item.item_type == "Product"
+				if @cart_item.item.product_options.any?
+					@cart_item.item.product_options.each do |opt|
+						option_id << opt.option_id
+					end
+				end
+			else
+
 			end
+
+			i = 0
+			while i < option_id.length
+				if params["option_#{option_id[i]}"].is_a? Array
+					params["option_#{option_id[i]}"].each do |opt|
+						@cart_item_option = CartItemOption.create(cart_item_id: @cart_item.id, option_item_id: opt.to_i)
+					end	
+				else
+					@cart_item_option = CartItemOption.create(cart_item_id: @cart_item.id, option_item_id: params["option_#{option_id[i]}"].to_i)	
+				end
+				i += 1
+			end
+			#add cart_item_option
+			# @cart_item_option = CartItemOption.create(cart_item_id: @cart_item.id, option_item_id: params[:option_1].to_i)
+			# @cart_item_option = CartItemOption.create(cart_item_id: @cart_item.id, option_item_id: params[:option_2].to_i)
+			# params[:option_3].each do |opt|
+			# 	@cart_item_option = CartItemOption.create(cart_item_id: @cart_item.id, option_item_id: opt.to_i)
+			# end
 
 			#get cart_item_option prices
 			@cart_item_price = @cart_item.price_cents

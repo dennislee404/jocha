88cf414e4a7cd25a98dc1000c2ce269621a27ef2
Dennis Lee <dennislee404@gmail.com>
2023-11-03 15:12:04 +0800

    trying seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+file = File.read(Rails.root.join('db','seeds','product.csv'))
+rows = CSV.parse(file, headers: true, encoding: 'utf-8')
+rows.each do |row|
+	category = Category.find_or_create_by_name(name: row['Category'])
+	product = category.products.find_or_create_by(name: row['Product'], price: row['Price'])
+end
+
+file = File.read(Rails.root.join('db','seeds','variant.csv'))
+rows = CSV.parse(file, headers: true, encoding: 'utf-8')
+rows.each do |row|
+	variant = Variant.find_or_create_by_name(name: row['Variant'])
+	variant_item = variant.variant_items.find_or_create_by(name: row['Variant Item'])
+end

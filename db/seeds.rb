5252f9cb6a50bc2cafd90788959580e0c8d2dc66
Dennis Lee <dennislee404@gmail.com>
2023-11-04 11:43:39 +0800

    seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,37 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'csv'
 
-# file = File.read(Rails.root.join('db','seeds','product.csv'))
-# rows = CSV.parse(file, headers: true, encoding: 'utf-8')
-# rows.each do |row|
-# 	category = Category.find_or_create_by(name: row['Category'])
-# 	product = category.products.find_or_create_by(name: row['Product'], price: row['Price'])
-# end
-
 file = File.read(Rails.root.join('db','seeds','variant.csv'))
 rows = CSV.parse(file, headers: true, encoding: 'utf-8')
 rows.each do |row|
 	variant = Variant.find_or_create_by(name: row['Variant'])
 	variant_item = variant.variant_items.find_or_create_by(name: row['Variant Item'])
+end
+
+file = File.read(Rails.root.join('db','seeds','option.csv'))
+rows = CSV.parse(file, headers: true, encoding: 'utf-8')
+rows.each do |row|
+	option = Option.find_or_create_by(name: row['Option'], min: row['Min'], max: row['Max'], stackable: row['Stackable'])
+	option_item = option.option_items.find_or_create_by(name: row['Option Item'], price: row['Price'])
+end
+
+file = File.read(Rails.root.join('db','seeds','product.csv'))
+rows = CSV.parse(file, headers: true, encoding: 'utf-8')
+rows.each do |row|
+	category = Category.find_or_create_by(name: row['Category'])
+	product = category.products.find_or_create_by(name: row['Product'], price: row['Price'])
+end
+
+file = File.read(Rails.root.join('db','seeds','product_variant.csv'))
+rows = CSV.parse(file, headers: true, encoding: 'utf-8')
+rows.each do |row|
+	product = Products.find_by(name: row['Product'])
+	product_variant = product.product_variants.find_or_create_by(variant_item_id: row['Variant Item'], price: row['Price'])
+end
+
+file = File.read(Rails.root.join('db','seeds','product_option.csv'))
+rows = CSV.parse(file, headers: true, encoding: 'utf-8')
+rows.each do |row|
+	product = Products.find_by(name: row['Product'])
+	product_option = product.product_options.find_or_create_by(option_id: row['Option'])
 end

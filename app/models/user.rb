080dae98c3e7e14a3aa0dcb3902eb2172907b358
dennislee404 <dennislee404@gmail.com>
2023-11-08 17:38:56 +0800

    asset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  #  :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  #  :lockable,  :confirmable,:timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :orders

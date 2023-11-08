9d08b0b587ad71c3f3ba93acfb313af4c8362b63
dennislee404 <dennislee404@gmail.com>
2023-11-08 17:23:47 +0800

    migration

--- db/migrate/20231102092032_change_price_column.rb
+++ db/migrate/20231102092032_change_price_column.rb
@@ -1,6 +1,6 @@
 class ChangePriceColumn < ActiveRecord::Migration[7.0]
   def change
-    remove_column :cart_items, :price    
+    remove_column :cart_items, :price, if_exists: true   
     add_column :cart_items, :price_cents, :integer
   end
 end
